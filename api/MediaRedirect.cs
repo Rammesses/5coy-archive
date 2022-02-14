993ea55b108626104e3abe8e2abdb0a4d835d759
Joel Hammond-Turner <joel@hammond-turner.org.uk>
2022-02-14 09:49:58 +0000

    Remove redundant build plan. Enable API build.

--- api/MediaRedirect.cs
+++ api/MediaRedirect.cs
@@ -23,7 +23,7 @@ namespace api
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            name = name ?? data?.name ?? "Sweetie";
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
