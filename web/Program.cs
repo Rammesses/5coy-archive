b56265fdbafc4141059da71600d8b5d24cfd567a
Joel Hammond-Turner <joel@hammond-turner.org.uk>
2022-02-14 16:07:12 +0000

    Add client-side AppInsights (not working yet)

--- web/Program.cs
+++ web/Program.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 using Services;
+using BlazorApplicationInsights;
 
 namespace web
 {
@@ -21,10 +22,20 @@ namespace web
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+            var config = builder.Build().Configuration;
+
             builder.Services.AddLogging(builder => builder
                 .AddBrowserConsole()
                 .SetMinimumLevel(LogLevel.Information)
             );
+
+            builder.Services.AddBlazorApplicationInsights(async applicationInsights =>
+            {
+                // var appInsightsKey = config["ApplicationInsights:InstrumentationKey"] ?? Guid.Empty.ToString();
+                var appInsightsKey = "181e8816-8264-4b24-9b0e-0951727e5c76";
+                await applicationInsights.SetInstrumentationKey(appInsightsKey);        
+                await applicationInsights.TrackPageView();
+            });
 
             builder.RootComponents.Add<App>("#app");
 
