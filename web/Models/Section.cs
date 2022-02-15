714bd74f72cc62d0c8b93debcb6bfec829ede183
Joel Hammond-Turner <joel@hammond-turner.org.uk>
2022-02-15 13:59:51 +0000

    Implement hierarchical sections

--- web/Models/Section.cs
+++ web/Models/Section.cs
@@ -1,14 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 namespace Models
 {   
-    public class Section 
+    public class Section : IHeirarchicalItem<Section>
     {
         public string Reference { get; set; }
         public string Title { get; set;}
         public string ContentUrl { get; set; }
         public string Glyph { get; set; }
+
         public string Url => $"/articles/{Reference}";
+
         public Article[] Articles { get; set; } = Array.Empty<Article>();
+
+        public Section[] Children { get; set; } = Array.Empty<Section>();
+
+        public Section Parent { get; set; }
+
+        public static IEnumerable<Section> Flatten (IEnumerable<Section> list)
+            => Extensions.EnumerableExtensions.Flatten<Section>(list, a => a.Children);
+
     }
 }
